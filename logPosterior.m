8af4234515c5e1aec83e78f087a05af21a21d7fd
umangU <72771903+umangU@users.noreply.github.com>
2021-03-21 12:39:19 +1100

    Code File Added

---
+++ logPosterior.m
@@ -0,0 +1,33 @@
+function [logpdf, gradlogpdf] = logPosterior(Parameters,X,Y,InterceptPriorMean,InterceptPriorSigma,BetaPriorMean,BetaPriorSigma,LogNoiseVarianceMean,LogNoiseVarianceSigma)
+
+
+% Unpack the parameter vector
+Intercept        = Parameters(1);
+Beta             = Parameters(2:end-1);
+LogNoiseVariance = Parameters(end);
+% Compute the log likelihood and its gradient
+Sigma                   = sqrt(exp(LogNoiseVariance));
+Mu                      = X*Beta + Intercept;
+Z                       = (Y - Mu)/Sigma;
+loglik                  = sum(-log(Sigma) - .5*log(2*pi) - .5*Z.^2);
+gradIntercept1          = sum(Z/Sigma);
+gradBeta1               = X'*Z/Sigma;
+gradLogNoiseVariance1	= sum(-.5 + .5*(Z.^2));
+% Compute log priors and gradients
+[LPIntercept, gradIntercept2]           = normalPrior(Intercept,InterceptPriorMean,InterceptPriorSigma);
+[LPBeta, gradBeta2]                     = normalPrior(Beta,BetaPriorMean,BetaPriorSigma);
+[LPLogNoiseVar, gradLogNoiseVariance2]  = normalPrior(LogNoiseVariance,LogNoiseVarianceMean,LogNoiseVarianceSigma);
+logprior                                = LPIntercept + LPBeta + LPLogNoiseVar;
+% Return the log posterior and its gradient
+logpdf               = loglik + logprior;
+gradIntercept        = gradIntercept1 + gradIntercept2;
+gradBeta             = gradBeta1 + gradBeta2;
+gradLogNoiseVariance = gradLogNoiseVariance1 + gradLogNoiseVariance2;
+gradlogpdf           = [gradIntercept;gradBeta;gradLogNoiseVariance];
+end
+
+function [logpdf,gradlogpdf] = normalPrior(P,Mu,Sigma)
+Z          = (P - Mu)./Sigma;
+logpdf     = sum(-log(Sigma) - .5*log(2*pi) - .5*(Z.^2));
+gradlogpdf = -Z./Sigma;
+end
